bfaf50becad9f8139755265b4e40208c7731643c
Clovis antunes <clovissantanna@gmail.com>
2023-06-29 16:42:21 -0300

    ajustando descrição

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -36,12 +36,10 @@ const CardProfile = () => {
           </div>
           <div className={styles.text}>
             <span>
-              Sou um desenvolvedor Front-End cujo interesse é construir sites
-              interativos e acessiveis. Minha sessão de projetos contem alguns
+              Sou um desenvolvedor Front-End com experiencia em next.js React.js, node.js e no consumo de api. Minha sessão de projetos contem alguns
               dos sites que ja criei. Atualmente estou aberto a oportunidades de
               trabalho, onde posso contribuir com a equipe estando sempre
-              disposto a melhorar e aprender mais. Entre em contato se estiver
-              interessado em me conhecer melhor.
+              disposto a melhorar e aprender mais.
             </span>
           </div>
 
