ec67eef02fc402e3640c98477321bec450717905
unknown <clovissantannaa@gmail.com>
2024-10-15 19:21:45 -0300

    fix fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document(){
     return(
         <Html>
             <Head>
-
+            <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
             </Head>
             <body>
                 <Main/>
